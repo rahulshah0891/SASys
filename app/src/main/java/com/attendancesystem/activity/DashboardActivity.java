96590f7c0521c2bf457867066be54a086403940e
rahulshah0891 <rahulshah0891@gmail.com>
2018-03-20 22:59:11 +0530

    Initial
    Initial

---
+++ app/src/main/java/com/attendancesystem/activity/DashboardActivity.java
@@ -0,0 +1,24 @@
+package com.attendancesystem.activity;
+
+import com.attendancesystem.R;
+
+/**
+ * Created by Rahul on 3/18/2018.
+ */
+
+public class DashboardActivity extends BaseActivity{
+    @Override
+    public int getContentView() {
+        return R.layout.activity_dashboard;
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+}
